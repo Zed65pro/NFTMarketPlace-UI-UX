2c530883908d6f16d95cd2a60fddf6dad039d553
Zaid Ibaisi <zed65pro@hotmail.com>
2023-07-26 20:02:02 +0300

    Sonarqube fixes + "Load more" function fix

--- client/src/components/MyNfts.tsx
+++ client/src/components/MyNfts.tsx
@@ -1,26 +1,29 @@
 import { useEffect, useState } from "react";
-import { setGlobalState, useGlobalState } from "../store";
-import { getGlobalState } from "../store/index";
+import { useGlobalState } from "../store";
+import { Card } from "./Card";
 
 const MyNfts = () => {
   const [nfts] = useGlobalState("nfts");
+  const [ownerNfts, setOwnerNfts] = useState<any[]>([]);
   const [account] = useGlobalState("connectedAccount");
   const [end, setEnd] = useState(4);
   const [count] = useState(4);
   const [collection, setCollection] = useState([]);
   const [key, setKey] = useState(0);
+
   const getCollection = () => {
     if (account) {
       const filteredNFTs = nfts.filter(
         (nft: any) => nft.owner.toLowerCase() === account.toLowerCase()
       );
+      setOwnerNfts(filteredNFTs);
       return filteredNFTs.slice(0, end);
     }
   };
 
   useEffect(() => {
     setCollection(getCollection());
-  }, [nfts, end, account]);
+  }, [nfts, end, account, key]);
 
   useEffect(() => {
     setKey((prevKey) => prevKey + 1);
@@ -33,19 +36,19 @@ const MyNfts = () => {
   if (!account) return <></>;
 
   return (
-    <div className="bg-[#151c25] gradient-bg-artworks">
+    <div className="bg-[#151c25] gradient-bg-artworks" id="myArtworks">
       <div className="w-4/5 py-10 mx-auto">
         <h4 className="text-white text-3xl font-bold uppercase text-gradient">
           {collection.length > 0 ? "My Artworks" : "No owned Artworks yet"}
         </h4>
 
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-4 lg:gap-3 py-2.5">
-          {collection.map((nft, i) => (
-            <Card key={i} nft={nft} />
+          {collection.map((nft: any, i) => (
+            <Card key={nft.id} nft={nft} />
           ))}
         </div>
 
-        {collection.length > 0 && nfts.length > collection.length ? (
+        {collection.length > 0 && ownerNfts.length > collection.length ? (
           <div className="text-center my-5">
             <button
               className="shadow-xl shadow-black text-white
@@ -57,45 +60,6 @@ const MyNfts = () => {
             </button>
           </div>
         ) : null}
-      </div>
-    </div>
-  );
-};
-
-const Card = ({ nft }: any) => {
-  const setNFT = () => {
-    const account = getGlobalState("connectedAccount");
-    if (!account) return alert("Metamask not connected amk");
-    setGlobalState("nft", nft);
-    setGlobalState("showModal", "scale-100");
-  };
-
-  return (
-    <div className="w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3">
-      <img
-        src={nft.metadataURI}
-        alt={nft.title}
-        className="h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3"
-      />
-      <h4 className="text-white font-semibold">{nft.title}</h4>
-      <p className="text-gray-400 text-xs my-1">{nft.description}</p>
-      <div className="flex justify-between items-center mt-3 text-white">
-        <div className="flex flex-col">
-          <small className="text-xs">Current Price</small>
-          {nft?.price > 0 ? (
-            <p className="text-sm font-semibold">{nft?.price} MATIC</p>
-          ) : (
-            <p className="text-sm font-semibold text-red-600">unavailable</p>
-          )}
-        </div>
-
-        <button
-          className="shadow-lg shadow-black text-white text-sm bg-[#e32970]
-            hover:bg-[#bd255f] cursor-pointer rounded-full px-1.5 py-1"
-          onClick={setNFT}
-        >
-          View Details
-        </button>
       </div>
     </div>
   );
